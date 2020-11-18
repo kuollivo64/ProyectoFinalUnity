021af4919710723b8f93a1a11eabf84432a9020b
kuollivo64 <44689881+kuollivo64@users.noreply.github.com>
2020-11-17 22:55:57 -0400

    Mejora de Nivel1
    -Se agrego enemigos
    -Se agrego mivimiento a los enemigos de ida y vuelta
    -Se agrego movimiento de plataformas del ultimo piso sube y baja
    -Se agrego pisos de fuego para las plataformas de sube y baja
    -Se agrego sonido ambiente para el nivel 1

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Header("Patrullaje")]
+    public float Speed;
+    public bool chocaOno;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (chocaOno)
+        {
+            transform.Translate(1 * Time.deltaTime * Speed, 0, 0);
+        }
+        else
+        {
+            transform.Translate(-1 * Time.deltaTime * Speed, 0, 0);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("cho"))
+        {
+            if (chocaOno)
+            {
+                chocaOno = false;
+            }
+            else
+            {
+                chocaOno = true;
+            }
+        }
+    }
+
+}
